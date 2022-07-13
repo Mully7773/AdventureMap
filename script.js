832676bc7a7a6605231c662d2ef4569e4cad61b5
Nick M <mully7773@gmail.com>
2022-07-13 15:45:30 -0500

    Added map to full viewport background instead of sidebar and moved position of zoom in and out button to top right of map

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ if (navigator.geolocation)
       //   console.log(latitude, longitude);
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      const map = L.map("map", { zoomControl: false }).setView(coords, 13);
+
+      L.control.zoom({ position: "topright" }).addTo(map);
 
       L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
         maxZoom: 20,
         subdomains: ["mt0", "mt1", "mt2", "mt3"],
       }).addTo(map);
-
-      //   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      //     attribution:
-      //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      //   }).addTo(map);
 
       L.marker(coords)
         .addTo(map)
