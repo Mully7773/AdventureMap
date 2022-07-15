4d19da47288c7842da517a1c85ec26852d7fb2f0
Nick M <mully7773@gmail.com>
2022-07-15 11:53:02 -0500

    Fixed form submission and marker display issue with absolutely positioned sidebar layout on top of map instead of grid layout

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const inputActivity = document.querySelector(".form-input--activity");
 const inputCost = document.querySelector(".form-input--cost");
 const adventureContainer = document.querySelector(".adventures");
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -15,7 +17,7 @@ if (navigator.geolocation)
       //   console.log(latitude, longitude);
       const coords = [latitude, longitude];
 
-      const map = L.map("map", { zoomControl: false }).setView(coords, 13);
+      map = L.map("map", { zoomControl: false }).setView(coords, 13);
 
       L.control.zoom({ position: "topright" }).addTo(map);
 
@@ -24,27 +26,35 @@ if (navigator.geolocation)
         subdomains: ["mt0", "mt1", "mt2", "mt3"],
       }).addTo(map);
 
-      map.on("click", function (mapEvent) {
+      map.on("click", function (mapE) {
+        console.log("hello");
+        mapEvent = mapE;
         form.classList.remove("hidden");
-        inputActivity.focus();
-        // console.log(mapEvent);
-        // const { lat, lng } = mapEvent.latlng;
-        // L.marker([lat, lng])
-        //   .addTo(map)
-        //   .bindPopup(
-        //     L.popup({
-        //       minWidth: 100,
-        //       maxWidth: 250,
-        //       autoClose: false,
-        //       closeOnClick: false,
-        //       className: `solo-popup`,
-        //     })
-        //   )
-        //   .setPopupContent("Adventure!")
-        //   .openPopup();
+        // inputActivity.focus();
       });
     },
     function () {
       alert("Could not get your position");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  //Display marker
+  // console.log(mapEvent);
+  e.preventDefault();
+  console.log("hello");
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        minWidth: 100,
+        maxWidth: 250,
+        autoClose: false,
+        closeOnClick: false,
+        className: `solo-popup`,
+      })
+    )
+    .setPopupContent("Adventure!")
+    .openPopup();
+});
