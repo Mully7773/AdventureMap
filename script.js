358dafe5e155faad38e09fbd2e05e70f97ad7949
Nick M <mully7773@gmail.com>
2022-07-15 15:05:44 -0500

    Added Solo, Family, and Friends classes along with test data

--- script.js
+++ script.js
@@ -9,14 +9,40 @@ const adventureContainer = document.querySelector(".adventures");
 
 class Adventure {
   date = new Date();
+  id = uuidv4(); // Unique ID
 
   constructor(coords, activity, cost, duration) {
-    this.coords = coords;
+    this.coords = coords; //Must be an array for leaflet: [lat, lng]
     this.activity = activity; //string
     this.cost = cost; // number
     this.duration = duration; // number in minutes
   }
 }
+
+class Solo extends Adventure {
+  constructor(coords, activity, cost, duration) {
+    super(coords, activity, cost, duration);
+  }
+}
+
+class Family extends Adventure {
+  constructor(coords, activity, cost, duration) {
+    super(coords, activity, cost, duration);
+  }
+}
+
+class Friends extends Adventure {
+  constructor(coords, activity, cost, duration) {
+    super(coords, activity, cost, duration);
+  }
+}
+
+const solo1 = new Solo([34, 52], "Bowling", 77, 120);
+const family1 = new Family([34, 52.5], "Camping", 200, 500);
+const friends1 = new Friends([34, 53], "Arcade", 85, 300);
+
+console.log(solo1, family1, friends1);
+
 class App {
   #map;
   #mapEvent;
